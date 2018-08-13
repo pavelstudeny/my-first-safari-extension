0bb18d7d8bd6297117a2cb2374b592eebc8e3b5a
Studený Pavel <studeny@avast.com>
2018-08-13 15:25:41 +0200

    inject an img in addition to the iframe

--- The Actual Extension/script.js
+++ The Actual Extension/script.js
@@ -1,10 +1,20 @@
 console.log('script.js injected into', window.location.href);
 
-document.addEventListener('DOMContentLoaded', function () {
-  console.log(location.href, 'opening page', safari.extension.baseURI + 'dialog.html')
-  var f = document.createElement('iframe');
-  f.src = safari.extension.baseURI + 'dialog.html';
-  document.body.appendChild(f);
-});
+if (document.body) {
+    injectIframe();
+}
+else {
+    document.addEventListener('DOMContentLoaded', injectIframe);
+}
 
+function injectIframe() {
+    console.log(location.href, 'opening page', safari.extension.baseURI + 'dialog.html')
+    
+    var i = document.createElement('img');
+    i.src = safari.extension.baseURI + 'icon32.png';
+    document.body.appendChild(i);
 
+    var f = document.createElement('iframe');
+    f.src = safari.extension.baseURI + 'dialog.html';
+    document.body.appendChild(f);
+}
